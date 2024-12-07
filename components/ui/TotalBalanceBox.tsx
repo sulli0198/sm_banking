1aa8c6e9615fe36648d55d9e98ccd5582449bff8
Salman Ibrahim <SalmanIbrahim521@gmail.com>
2024-12-07 11:28:02 +0200

    create the totalbalance box t odisplay current balance and stuff

--- components/ui/TotalBalanceBox.tsx
+++ components/ui/TotalBalanceBox.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
-const TotalBalanceBox = () => {
+const TotalBalanceBox = ({
+  accounts = [] , totalBanks ,totalCurrentBalance 
+}: TotlaBalanceBoxProps) => {
   return (
-    <div>
-      total bal
-    </div>
+    <section className='total-balance'>
+      <div className='total-balance-chart'>
+      {/*dougnut chart*/}
+      </div>
+
+      <div className='flex flex.col gap-6'>
+        <h2 className='header-2'>
+        Bank Accounts : {totalBanks} 
+        </h2>
+        <div className='flex flex-col gap-2'>
+            <p className='total-balance-label'>
+              Total Current Balance
+            </p>
+            <p className='total-balance-amount flex-center gap-2'>
+              {formatAmount(totalCurrentBalance)}
+            </p>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
