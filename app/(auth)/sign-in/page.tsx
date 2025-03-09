acd86675dfaada80c33a8d2aeacc5e923482f989
Salman Ibrahim <SalmanIbrahim521@gmail.com>
2025-03-09 20:17:59 +0200

    add new fields to the form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const SignIn = () => {
   return (
     <section className='flex-center size-full max-sm:px-6'>
-      <AuthForm type="sign-ins" />
+      <AuthForm type="sign-in" />
     </section>
   );
 };
--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -16,7 +16,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Divide, Loader2 } from 'lucide-react';
+import { Divide, Link2, Loader2 } from 'lucide-react';
 import { authFormSchema } from '@/lib/utils';
 import CustomInput from './CustomInput';
 
@@ -65,7 +65,7 @@ const AuthForm = ({ type }: { type: string }) => {
           <h1 className='text-24 lg:text-36 font-semibold text-white'>
             {user
              ? 'Link Account'
-            : type=== 'sign-ins'
+            : type === 'sign-in'
                ? 'Sign-in'
                : 'Sign-up' }
             <p className='text-16 font-normal'>
@@ -86,6 +86,47 @@ const AuthForm = ({ type }: { type: string }) => {
           <>
            <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === 'sign-up' && (
+                <>
+                 <CustomInput 
+                  control={form.control} 
+                  name='firstName' 
+                  label="First name" 
+                  placeholder='Enter your first name'
+                  />
+                  <CustomInput 
+                  control={form.control} 
+                  name='lastName' 
+                  label="Last name" 
+                  placeholder='Enter your last name'
+                  />
+                  <CustomInput 
+                  control={form.control} 
+                  name='address' 
+                  label="Address" 
+                  placeholder='Enter your address'
+                  />
+                  <CustomInput 
+                  control={form.control} 
+                  name='state' 
+                  label="State" 
+                  placeholder='Example: NY'
+                  />
+                  <CustomInput 
+                  control={form.control} 
+                  name='postalCode' 
+                  label="Postal Code" 
+                  placeholder='Examle: 11101'
+                  />
+                  <CustomInput 
+                  control={form.control} 
+                  name='dateOfBirth' 
+                  label="Date of Birth" 
+                  placeholder='YYYY-MM-DD'
+                  />
+                </>
+              )}
+
               <CustomInput 
               control={form.control} 
               name='email' 
@@ -94,22 +135,33 @@ const AuthForm = ({ type }: { type: string }) => {
               />
               <CustomInput 
               control = {form.control} 
-              name = 'password' 
+              name = 'password'
               label = "Password" 
               placeholder = 'Enter your password'
               />
-
-
-              <Button className='form-btn' type="submit">
-                { isLoading ? (
-                  <>
-                  <Loader2 size = {20} className='animate-spin' /> &nbsp;
-                  Loading...
-                  </>
-                ): type === 'sign-in' ? 'Sign In' : 'Sign Up' }
-              </Button>
+              <div className='flex flex-col gap-4'>
+                <Button className='form-btn' type="submit" disabled={isLoading}>
+                  { isLoading ? (
+                    <>
+                    <Loader2 size = {20} className='animate-spin' /> &nbsp;
+                    Loading...
+                    </>
+                  ): type === 'sign-in' ? 'Sign In' : 'Sign Up' }
+                </Button>
+              </div>
             </form>
           </Form>
+          <footer className='flex justify-center gap-1'>
+            <p className='text-14 font-normal'>
+              {type === 'sign-in' ? "Don't have an account?" : "Already have an account?"}
+            </p>
+            <Link 
+              className='form-link' 
+              href={type === 'sign-in' ? '/sign-up' : '/sign-in'}
+            >
+              {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+            </Link>
+          </footer>
           </>
         )
         }
