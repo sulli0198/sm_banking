8d73abaaeef7cbef5e8ca1b35a432101dd157983
Salman Ibrahim <SalmanIbrahim521@gmail.com>
2025-03-30 00:45:06 +0200

    get the user from sign-up

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -67,12 +67,12 @@ const AuthForm = ({ type }: { type: string }) => {
       }
 
       if(type === 'sign-in'){
-        const response = await signIn({
-          email: data.email,
-          password: data.password,
-        });
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // });
 
-        if(response) router.push('/')
+        // if(response) router.push('/')
       }
 
       console.log(values) 
